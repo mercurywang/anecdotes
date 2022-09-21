e8c08d289d44dc646fe948354bf498bd29dabee9
mercury_wang <mercwang@hotmail.com>
2022-09-21 20:08:08 +0900

    feat: exer 6.19

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,14 +1,22 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notifications)
-
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
-  return notification ? <div style={style}>{notification}</div> : null
+  return props?.notifications ? (
+    <div style={style}>{props.notifications}</div>
+  ) : null
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notifications: state.notifications
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
